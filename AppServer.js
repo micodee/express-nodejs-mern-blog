9b8a83b9ba10b1f42d85c481347a0ea72937554b
micodee <martomhidayat>
2022-12-28 15:03:48 +0700

    20 - Basic Router, Request dan Response di NodeJS

--- AppServer.js
+++ AppServer.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+const router = express.Router();
+router.use("/users", (req, res, next) => {
+  res.json({
+    name: "marcelino hidayat",
+    email: "marcelino@gmail.com",
+  });
+  next();
 });
+
+router.use("/price", (req, res, next) => {
+  res.json({
+    price: 900000
+  });
+  next();
+});
+
+router.get("/customer", (req, res, next) => {
+  res.json({
+    title: "customer"
+  });
+  next();
+});
+
+app.use("/", router);
+
+// methods get
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
 
 const port = 3000;
 app.listen(port, () => console.log(`running on http://localhost:${port}`));
